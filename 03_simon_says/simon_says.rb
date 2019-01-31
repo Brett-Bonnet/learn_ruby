1b40968d50d3759760681f4435daeae740326c9d
Brett Bonnet <brettbonnet4@gmail.com>
2019-01-31 13:10:39 -0500

    Update simon_says.rb

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,26 @@
-#write your code here
+
+
+def echo(word)
+  puts word
+end
+
+def shout(word)
+puts word.upcase
+end  
+  
+def repeat(word, num=2)
+  num.times do
+    puts word
+  end  
+end
+
+def start_of_word(word, num)
+  puts word.slice(0..(num-1))
+end  
+
+
+def first_word 
+end
+
+def titleize 
+end
