5e79ff58e5003f1c1d7c678bdc37079f286629b9
Brett Bonnet <brettbonnet4@gmail.com>
2019-01-31 10:58:01 -0500

    Update calculator.rb

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,5 +1,4 @@
 
-
 def add(a, b)
 total = a + b
 end
@@ -15,4 +14,26 @@ def sum(array)
  puts total
 end
 
+def sum(array)
+ total = 0
+ total = array.inject(:+)
+ puts total
+end
 
+def multiply(array)
+  
+total = array.inject(1) {|product, n| product * n} 
+puts total  
+
+end
+
+def power(a, b)  
+ total = a ** b
+ puts total
+end
+
+def factorial(x)
+arr = (1..x).to_a
+total = arr.inject(1) {|product, n| product * n}
+puts total
+end  
