a3c5b644e83c37e60f0986e9793a7634bf31c9fe
Brett Bonnet <brettbonnet4@gmail.com>
2019-02-04 18:23:26 -0500

    Update book.rb

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,13 @@
 class Book
-# write your code here
+
+attr_reader :title
+
+def title=(words)
+  array = words.split(' ')
+  nocaps = ["and", "the", "over", "an", "a", "in", "of"]
+  new_array = array.map { |word| nocaps.include?(word)? word : word.capitalize }.join(' ')
+  book_title = new_array.sub(new_array[0], new_array[0].capitalize)
+  @title = book_title
 end
+end
+
