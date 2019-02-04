67d0d6000f2746823b361187498cb72b285118bb
Brett Bonnet <brettbonnet4@gmail.com>
2019-02-04 08:11:29 -0500

    Update timer.rb

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,14 @@
 class Timer
-  #write your code here
+attr_accessor :seconds
+
+def initialize
+@seconds = 0
+end
+
+def time_string
+  hours = @seconds / 3600
+  remainder = @seconds % (60 * 60)
+  sprintf("%02d:%02d:%02d", hours, remainder/60, remainder%60)
+end
+
 end
