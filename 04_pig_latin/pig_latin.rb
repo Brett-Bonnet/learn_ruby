2b1d850642b48f9f8f8d2068f563f3be2036893d
Brett Bonnet <brettbonnet4@gmail.com>
2019-02-04 18:23:57 -0500

    Update pig_latin.rb

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,19 @@
-#write your code here
+
+def translate(s)
+  s.split(' ').map do |string|
+  vow = for_each(string)
+  string.slice(vow..-1) + string[0,vow] + "ay"
+  end.join(' ')
+end
+
+def for_each(word)
+vowel = word.index(/[aeiou]/)
+which_vowel = word[vowel]
+if which_vowel == 'u' then
+ vowel+=1
+ else 	
+ vowel
+ end
+end
+
+
